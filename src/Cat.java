1a5e473c163d809c3c2113f8b235457018a67af2
Raman Kryvasheyeu <kryvasheyeu@gmail.com>
2016-12-28 14:57:35 +0300

    Commit changes. Added code for lesson 5 h/w.

--- src/Cat.java
+++ src/Cat.java
@@ -12,6 +12,6 @@ public class Cat extends Animal {
 
     @Override
     public void swim() {
-        System.out.println(name+" don't want to swim!");
+        System.out.println(name + " don't want to swim!");
     }
 }
