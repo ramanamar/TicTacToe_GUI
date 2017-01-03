d47a5ff600d07d10ea55b718b93ae4c56150b74a
Raman Kryvasheyeu <kryvasheyeu@gmail.com>
2017-01-03 17:59:06 +0300

    Added MyWindow class.

---
+++ src/MyWindow.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+/**
+ * Created by Raman on 03/01/2017.
+ */
+public class MyWindow extends JFrame {
+    public MyWindow(){
+        setTitle("Tic-Tac-Toe");
+        setBounds(800, 300, 400, 400);
+
+        setVisible(true);
+    }
+}
